4e38a49d8206771b2433f09dccd57c0a6883f952
Nexvis Mac Mini <khonkimhong101@gmail.com>
2025-12-07 10:50:19 +0700

    no message

--- dbs/db.py
+++ dbs/db.py
@@ -6,8 +6,11 @@ from sqlalchemy.orm import Session, sessionmaker
 DB_HOST = "localhost"
 DB_NAME = "student_db"
 DB_USER = "postgres"
-DB_PASSWORD = "123"
-DB_PORT = 5432
+# DB_PASSWORD = "123"
+# DB_PORT = 5432
+
+DB_PASSWORD = "root"
+DB_PORT = 8080
 
 URL_DATABASE = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(URL_DATABASE)
